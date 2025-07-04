a6c2baa1f62fea903501b6563c4c4b4c6deaa531
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-04 21:24:43 +0200

    use the serveError() helper and notFound() helper in the home handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -34,21 +34,19 @@ func (app *application) home(res http.ResponseWriter, req *http.Request) {
 	//the templates in a template set
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(res, "Internal Server Error", 500)
+		app.serverError(res, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(res, "base", nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(res, "Internal Server Error", 500)
+		app.serverError(res, err)
 	}
 }
 func (app *application) snippetView(res http.ResponseWriter, req *http.Request) {
 	id, err := strconv.Atoi(req.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(res, req)
+		app.notFound(res)
 		return
 	}
 	fmt.Fprintf(res, "Display a specific snippet with ID %d...", id)
--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -16,6 +16,7 @@ func (app *application) clientError(res http.ResponseWriter, status int) {
 	http.Error(res, http.StatusText(status), status)
 }
 
+// this helper is a convenience wrapper aroud clientError which sends a 404 Not Found response
 func (app *application) notFound(res http.ResponseWriter) {
 	app.clientError(res, http.StatusNotFound)
 }
