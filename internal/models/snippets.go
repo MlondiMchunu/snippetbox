bb1240c98164ced19743a169e57d8b2f82449847
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-05 16:33:04 +0200

    define a SnippetModel type which wraps a sql.DB connection pool

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -1,11 +1,19 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
+/*Define a Snippet type to hold the data for an individual snippet*/
 type Snippet struct {
 	ID      int
 	Title   string
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
 }
