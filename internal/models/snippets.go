ebce7772e85741112860937d3258299ebb318b2d
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-05 16:56:31 +0200

    define database connection function

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -14,6 +14,17 @@ type Snippet struct {
 	Expires time.Time
 }
 
+// define a SnippetModel type which wraps a sql.DB connection pool
 type SnippetModel struct {
 	DB *sql.DB
+}
+
+// this will insert a new snippeet into the database
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 0, nil
+}
+
+// this will return a specific snippet based on its id
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
 }
