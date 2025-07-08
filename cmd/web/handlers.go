69a8f8fd7c763f2b276db1219a721e534a83768d
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-08 14:23:31 +0200

    refactor id in snippetview

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,26 +44,11 @@ func (app *application) home(res http.ResponseWriter, req *http.Request) {
 	}
 }
 func (app *application) snippetView(res http.ResponseWriter, req *http.Request) {
-	id1, err := strconv.Atoi(req.URL.Query().Get("id1"))
-	if err != nil || id1 < 1 {
+	id, err := strconv.Atoi(req.URL.Query().Get("id"))
+	if err != nil || id < 1 {
 		app.notFound(res)
 		return
 	}
-
-	/**declare variables and assign dummy data*/
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
-
-	// Pass the data to the SnippetModel.Insert() method, receiving the
-	// ID of the new record back.
-	id, err := app.snippets.Insert(title, content, expires)
-	if err != nil {
-		app.serverError(res, err)
-		return
-	}
-	//Redirect the user to the relevant page for the snippet
-	http.Redirect(res, req, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 
 	fmt.Fprintf(res, "Display a specific snippet with ID %d...", id)
 
@@ -99,7 +84,6 @@ func (app *application) snippetCreate(res http.ResponseWriter, req *http.Request
 
 	//redirect the user to the relevant page for the snippet
 	http.Redirect(res, req, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
-
 	res.Write([]byte("Create a new snippet..."))
 	res.Header().Set("Allow", "POST")
 }
