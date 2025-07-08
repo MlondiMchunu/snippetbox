1748ae81034fed203dd9ebbec90c605fe8913947
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-08 14:53:05 +0200

    initialize a pointer to a new zeroed Snippet struct

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -46,4 +46,7 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
 	row := m.DB.QueryRow(stmt, id)
+
+	//initialize a pointer to a new zeroed Snippet struct
+	s := &Snippet{}
 }
