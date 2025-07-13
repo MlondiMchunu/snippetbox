6ad8ca03edef9aae41def3c562503f8bcf443904
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-14 00:33:24 +0200

    initialize the filepath.Glob() function to get a slice of all filepaths that match the patthern ./ui/html/pages/*.tmpl

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"path/filepath"
 
 	"snippetbox.mlodev.net/internal/models"
 )
@@ -14,5 +15,10 @@ type templateData struct {
 func newTemplateCache() (map[string]*template.Template, error) {
 	//initialize a new map to act as a cache
 	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
 
 }
