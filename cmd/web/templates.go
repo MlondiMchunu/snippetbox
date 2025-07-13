dec670cfdbb538f4dfdd673d402ce4f1aac83254
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-14 00:30:09 +0200

    create an in-memory map to cache the parsed templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"html/template"
+
 	"snippetbox.mlodev.net/internal/models"
 )
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	//initialize a new map to act as a cache
+	cache := map[string]*template.Template{}
+
 }
