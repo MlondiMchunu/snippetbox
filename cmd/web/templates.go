8810538938bd94dd231a14b8ecd742f4f2bf905f
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-14 10:25:13 +0200

    extract the file name from the full filepath

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -16,9 +16,15 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	//initialize a new map to act as a cache
 	cache := map[string]*template.Template{}
 
+	//gets a slice of all filepaths that match the pattern ./ui/html/pages/*.tmpl
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
+	}
+
+	//loop through the page filepaths one-by-one
+	for _, page := range pages {
+		name := filepath.Base(page)
 	}
 
 }
