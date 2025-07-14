e8b1d03d5577c411b69604ad44a327b5610c05d0
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-14 22:57:09 +0200

    execute template set and write the response body

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -32,4 +32,10 @@ func (app *application) render(res http.ResponseWriter, status int, page string,
 
 	//write the provided HTTP status code
 	res.WriteHeader(status)
+
+	//execute the template set and write response body
+	err := ts.ExecuteTemplate(res, "base", data)
+	if err != nil {
+		app.serverError(res, err)
+	}
 }
