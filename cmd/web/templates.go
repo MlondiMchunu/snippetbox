f686fafe44b256234236d57c7f2bb04d7ddcbdcb
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-18 11:30:49 +0200

    initialize a template.FuncMap object

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -66,3 +66,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 func humanDate(t time.Time) string {
 	return t.Format("02 Jan 2006 at 15:04")
 }
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
