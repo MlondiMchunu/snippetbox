b5ce5951f1c013ae025465f6505a09e29123c690
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-07-31 19:52:56 +0200

    use ListenAndServe() method to start HTTPS server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -133,7 +133,7 @@ func main() {
 
 	//local connection
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 
 }
