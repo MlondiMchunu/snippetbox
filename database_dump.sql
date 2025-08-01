17fdf33e0cabfc3aeaf9f47c559bb96bd10bed33
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-02 01:36:01 +0200

    commit db dump

--- database_dump.sql
+++ database_dump.sql
@@ -37,6 +37,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+INSERT INTO `sessions` VALUES ('EGMI2i7sRbXE17W7OQTwpW4knRS0Ghyv3FK9oj4XK34',_binary '%ˇÄ\0DeadlineˇÇ\0ValuesˇÑ\0\0\0ˇÅTimeˇÇ\0\0\0\'ˇÉmap[string]interface {}ˇÑ\0\0\0ˇÄ\0\0\0\‡ôÄ:ëˇˇ\0\0','2025-08-02 03:28:54.192953');
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +56,7 @@ CREATE TABLE `snippets` (
   `expires` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_snippets_created` (`created`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,8 +65,35 @@ CREATE TABLE `snippets` (
 
 LOCK TABLES `snippets` WRITE;
 /*!40000 ALTER TABLE `snippets` DISABLE KEYS */;
-INSERT INTO `snippets` VALUES (1,'An old silent pond','An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n‚Äì Matsuo Bash≈ç','2025-07-05 15:21:35','2026-07-05 15:21:35'),(2,'Over the wintry forest','Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n‚Äì Natsume Soseki','2025-07-05 15:21:37','2026-07-05 15:21:37'),(3,'First autumn morning','First autumn morning\nthe mirror I stare into\nshows my father\'s face.\n\n‚Äì Murakami Kijo','2025-07-05 15:21:54','2025-07-12 15:21:54'),(8,'o Snail','O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n‚Äì Kobayashi Issa','2025-07-22 14:25:31','2025-07-29 14:25:31'),(9,'Hello from Golang','package main\r\n\r\n  import (\"fmt\")\r\n\r\nfunc main(){\r\n  fmt.Println(\"First Go Hello\")\r\n}\r\n','2025-07-22 23:27:31','2026-07-22 23:27:31'),(11,'Sunday Code','Sunday I rest ','2025-07-27 20:46:00','2026-07-27 20:46:00'),(12,'klkj','hhuh','2025-07-27 20:46:48','2026-07-27 20:46:48'),(13,'go dev','I am a Golang developer','2025-07-30 12:00:23','2026-07-30 12:00:23');
+INSERT INTO `snippets` VALUES (1,'An old silent pond','An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n‚Äì Matsuo Bash≈ç','2025-07-05 15:21:35','2026-07-05 15:21:35'),(2,'Over the wintry forest','Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n‚Äì Natsume Soseki','2025-07-05 15:21:37','2026-07-05 15:21:37'),(3,'First autumn morning','First autumn morning\nthe mirror I stare into\nshows my father\'s face.\n\n‚Äì Murakami Kijo','2025-07-05 15:21:54','2025-07-12 15:21:54'),(8,'o Snail','O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n‚Äì Kobayashi Issa','2025-07-22 14:25:31','2025-07-29 14:25:31'),(9,'Hello from Golang','package main\r\n\r\n  import (\"fmt\")\r\n\r\nfunc main(){\r\n  fmt.Println(\"First Go Hello\")\r\n}\r\n','2025-07-22 23:27:31','2026-07-22 23:27:31'),(11,'Sunday Code','Sunday I rest ','2025-07-27 20:46:00','2026-07-27 20:46:00'),(12,'klkj','hhuh','2025-07-27 20:46:48','2026-07-27 20:46:48'),(13,'go dev','I am a Golang developer','2025-07-30 12:00:23','2026-07-30 12:00:23'),(14,'hello','print hello world message','2025-07-30 20:54:59','2025-08-06 20:54:59'),(15,'go message','Helloe\\n form \\n golang','2025-07-30 20:57:21','2026-07-30 20:57:21'),(16,'flash message test','this is for testing of \r\n\r\na flash message for \r\n\r\nsnippet creation\r\n\r\nby: <em>Mlondi Mchunu</em>','2025-07-31 10:33:43','2026-07-31 10:33:43'),(17,'session snippet','THis snippet is used to test sessions\r\n\r\nin the application','2025-08-01 17:28:54','2025-08-08 17:28:54');
 /*!40000 ALTER TABLE `snippets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hashed_password` char(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_uc_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -77,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-30 15:38:29
+-- Dump completed on 2025-08-02  1:24:13
