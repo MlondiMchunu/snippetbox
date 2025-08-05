04979dc35d1381136be451c35851acb2f0bc82cf
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-05 16:00:34 +0200

    validate email and password provision by user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -190,6 +190,17 @@ func (app *application) userLoginPost(res http.ResponseWriter, req *http.Request
 		app.clientError(res, http.StatusBadRequest)
 		return
 	}
+
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+
+	if !form.Valid() {
+		data := app.newTemplateData(req)
+		data.Form = form
+		app.render(res, http.StatusUnprocessableEntity, "login.tmpl", data)
+		return
+	}
 }
 
 func (app *application) userLogoutPost(res http.ResponseWriter, req *http.Request) {
