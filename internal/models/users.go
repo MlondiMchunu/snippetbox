97f393f579dbb042aaf2c12a7264c7e039c43327
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-05 12:07:36 +0200

    rretrieve id and hashed password

--- internal/models/users.go
+++ internal/models/users.go
@@ -44,6 +44,21 @@ VALUES(?,?,?,UTC_TIMESTAMP())`
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
+
+	var id int
+	var hashedPassword []byte
+
+	stmt := `SELECT id, hashed_password FROM users WHERE email = ?`
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
 	return 0, nil
 }
 
