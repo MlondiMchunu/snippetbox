6e625d346408c3e719b7f51e943423f8ec55667e
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-05 12:11:33 +0200

    check if hashedpassword and plain text password match

--- internal/models/users.go
+++ internal/models/users.go
@@ -58,8 +58,16 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 			return 0, err
 		}
 	}
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	return id, nil
 
-	return 0, nil
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
