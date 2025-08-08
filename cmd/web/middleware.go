8d6ca53eaf6d8f6a899694af5f629a6176ecbfaa
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-08 22:55:53 +0200

    create NoSurf middleware function

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func secureHeaders(next http.Handler) http.Handler {
@@ -47,4 +49,14 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 		res.Header().Add("CacheControl", "no-store")
 		next.ServeHTTP(res, req)
 	})
+}
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+	return csrfHandler
 }
