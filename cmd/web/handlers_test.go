ccd869a2af5a6bc12ac0c831bd72ded8e93e21b6
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-14 23:30:58 +0200

    initialize a new dummy http.Request

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
 	rr := httptest.NewRecorder()
+
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 }
