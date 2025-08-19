fa6cfc7b609d8e2d9c5019bc7bb1367bd866e4d1
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-08-19 22:44:35 +0200

    create changePasswordForm struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -31,6 +31,12 @@ type userLoginForm struct {
 	validator.Validator `form:"-"`
 }
 
+type changePasswordForm struct{
+	CurrentPassword 	string `form:"currentPassword"`
+	NewPassword 		string `form:"newPassword"`
+	ConfirmNewPassword 	string `form:"confirmNewPassword"`
+}
+
 // define home handler functions i.e controller which writes a byte slice containing
 func (app *application) home(res http.ResponseWriter, req *http.Request) {
 
@@ -248,6 +254,8 @@ func ping(res http.ResponseWriter, req *http.Request) {
 }
 
 func (app *application) accountPassowrdUpdate(res http.ResponseWriter, req *http.Request) {
+	data := app.newTemplateData(req)
+	data.Form = 
 
 }
 func (app *application) accountPassowrdUpdatePost(res http.ResponseWriter, req *http.Request) {
